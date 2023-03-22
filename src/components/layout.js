c3e0bd17e0e94ea06341f0c37784bee9d6296772
Daniel <daosan.nmd@gmail.com>
2023-03-22 18:58:11 +0000

    top bar menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,21 +4,13 @@ import { Head } from "gatsby-theme-landing-page";
 import "../styles.css";
 import * as cssVars from "gatsby-theme-landing-page/src/styles/variables.module.css";
 import * as styles from "./layout.module.css";
+import logo from "../assets/logo.png"
 
 export default function Layout(props) {
   return (
     <div className={[cssVars.root, styles.root].join(" ")}>
       <Head {...props} />
-      <header
-        style={{
-          background: `#fff`,
-          marginBottom: `1.45rem`,
-          display: `flex`,
-          alignItems: `center`,
-          justifyContent: `space-between`,
-          padding: `1.45rem 1.0875rem`,
-        }}
-      >
+      <header className={styles.header}>
         <div style={{ display: `flex`, alignItems: `center` }}>
           <Link to="/">
             <img src={logo} alt="Logo" style={{ height: `50px`, marginRight: `1rem` }} />
@@ -30,12 +22,8 @@ export default function Layout(props) {
           <Link to="/#testemunhos" style={{ marginRight: `1rem` }}>Testemunhos</Link>
           <Link to="/#contatos" style={{ marginRight: `1rem` }}>Contatos</Link>
         </div>
+    
       </header>
-      /*<header className={styles.header}>
-        <Link to="/">achei vc!!  
-          <a href="https://wa.me/5581998373767?text=Oi%2C%20O%20link%20chamou%20minha%20atencao%20e%20eu%20gostaria%20de%20saber%20mais%20sobre%20como%20criar%20uma%20landing%20page%20perfeita">WHATSAPP -></a>
-        </Link>
-      </header>*/
       <main className={styles.main}>{props.children}</main>
       <footer className={styles.footer}>
         <a href="https://gatsbyjs.com">Built with Gatsby</a>
